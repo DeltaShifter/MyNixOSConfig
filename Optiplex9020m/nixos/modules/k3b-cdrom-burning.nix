7f4dd24eeaae6e4bcb5fb6eeb5b41402e9e235d5
dale <dale@e.e>
2026-01-23 16:40:58 +0800

    1

--- Optiplex9020m/nixos/modules/k3b-cdrom-burning.nix
+++ Optiplex9020m/nixos/modules/k3b-cdrom-burning.nix
@@ -1,6 +1,30 @@
-{ config, pkgs, lib, ...}:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
-  programs.kdePackages.k3b.enable = true;
+{
+  options.programs.k3b = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = ''
+        Whether to enable k3b, the KDE disk burning application.
+
+        Additionally to installing `k3b` enabling this will
+        add `setuid` wrappers in `/run/wrappers/bin`
+        for both `cdrdao` and `cdrecord`. On first
+        run you must manually configure the path of `cdrdae` and
+        `cdrecord` to correspond to the appropriate paths under
+        `/run/wrappers/bin` in the "Setup External Programs" menu.
+      '';
+    };
+  };
+
+  config = lib.mkIf config.programs.k3b.enable {
+
     environment.systemPackages = with pkgs; [
       kdePackages.k3b
       dvdplusrwtools
