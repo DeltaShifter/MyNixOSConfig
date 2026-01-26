e9470df4599f0f3af50c777f394828e34ab75a4c
dale <dale@e.e>
2026-01-26 13:28:57 +0800

    1

--- generic/modules/fish.nix
+++ generic/modules/fish.nix
@@ -5,8 +5,8 @@
     enable = true;
     interactiveShellInit = "zoxide init fish | source";
     shellAliases = {
-      nixadd = "sudo nano +(math (grep -n '# ---PkgsEnd---' /etc/nixos/modules/programs.nix | cut -d: -f1) - 1) /etc/nixos/modules/programs.nix";
-      nixupd = "nh os switch /etc/nixos/";
+      nixadd = "hx ~/.MyNixConf/generic/modules/programs.nix:(math (grep -n '# ---PkgsEnd---' ~/.MyNixConf/generic/modules/programs.nix | cut -d: -f1) - 1)";
+      nixupd = "nh os switch ~/.MyNixConf/";
       nixclean = "sudo nix-collect-garbage -d";
       proxyon = "export http_proxy=http://127.0.0.1:20172 https_proxy=http://127.0.0.1:20172 && curl -I --connect-timeout 3 https://www.google.com";
     };
