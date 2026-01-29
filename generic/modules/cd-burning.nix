7e102436f4385d583f55df99d7ebde781d761fe5
dale <dale@e.e>
2026-01-29 13:42:57 +0800

    Normal update

---
+++ generic/modules/cd-burning.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, ... }:  
+
+{
+  environment.systemPackages = with pkgs; [
+    xfburn
+    xorriso 
+  ];
+
+  security.wrappers = {
+    xorrecord = {
+      setuid = true;
+      owner = "root";
+      group = "cdrom";
+      permissions = "u+wrx,g+x";
+      source = "${pkgs.xorriso}/bin/xorrecord";
+    };
+  };
+
+  users.groups.cdrom = {};
+}
