24aba64ab4b5ebcbbbb4e6c223c4d1c563da0894
dale <dale@email.e>
2026-02-02 00:30:55 +0800

    Normal update

--- generic/modules/dms-with-greeter.nix
+++ generic/modules/dms-with-greeter.nix
@@ -7,10 +7,35 @@
   ];
 
   programs.dank-material-shell.greeter = {
-    enable = true;
+    enable = false;
     compositor.name = "niri";  # Or "hyprland" or "sway"
   };
-
+  
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = ''
+        ${pkgs.tuigreet}/bin/tuigreet \
+          --time \
+          --remember \
+          --remember-user \
+          --asterisks \
+          --user-menu \
+          --container-padding 2 \
+          --width 40 \
+          --greeting "ThinkPad X1 Carbon | Ready for Action" \
+          --theme "border=blue;text=white;prompt=cyan;time=magenta;action=blue;button=white" \
+          --cmd niri
+      '';
+        user = "greeter";
+      };
+    };
+  };
+  systemd.services.greetd.serviceConfig = {
+  Environment = "LANG=en_US.UTF-8";
+  };
+  
   services.upower.enable = true;
 
   programs.dms-shell = {
