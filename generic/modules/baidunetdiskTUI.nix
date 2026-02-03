adc3f5b6dbaad4ab0d5931b02dbe30b67c7f3474
dale <dale@email.e>
2026-02-04 00:40:41 +0800

    Normal update

--- generic/modules/baidunetdiskTUI.nix
+++ generic/modules/baidunetdiskTUI.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 
-let
-  baidupcs-tui = pkgs.writeShellScriptBin "baidupcs-tui" (builtins.readFile ../scripts/baiduPCS.sh);
-in
 {
-  environment.systemPackages = [ baidupcs-tui ];
-
-  xdg.desktopEntries.baidupcs-tui = {
-    name = "百度网盘 TUI";
-    exec = "alacritty bash -c baidupcs-tui";
-    terminal = false;
-  };
+  environment.systemPackages = [
+    (pkgs.makeDesktopItem {
+      name = "baidupcs-tui";
+      desktopName = "百度网盘 TUI";
+      exec = "alacritty -e ${../scripts/baiduPCS.sh}";
+      icon = "baidu";
+      categories = [ "Network" ];
+      terminal = false;
+    })
+  ];
 }
