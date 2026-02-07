b720edc2fc0dcb0602bc3f889b3136da961562c4
dale <dale@e.e>
2026-02-08 00:42:11 +0800

    Normal update

--- generic/modules/rofi.nix
+++ generic/modules/rofi.nix
@@ -1,27 +1,34 @@
 { pkgs , ... }:
 
-let
-  givemecustomPOWER = pkgs.writeShellScriptBin "dale-mod-power-menu"  ''
-  ${pkgs.rofi-power-menu}/bin/rofi-power-menu "$@" | sed \
-    -e 's/.*prompt.*Power menu.*/prompt\t⏻ 电源/' \
-    -e 's/>Shut down</>关机</g' \
-    -e 's/>Reboot</>重启</g' \
-    -e 's/>Suspend</>挂起</g' \
-    -e 's/>Hibernate</>休眠</g' \
-    -e 's/>Log out</>注销</g' \
-    -e 's/>Lock screen</>锁屏</g'
-  '';
-in
 {
   environment.systemPackages = with pkgs;[
     (rofi.override{
       plugins = [
         rofi-calc
         rofi-pass
-        rofimoji
-        givemecustomPOWER
       ];
     })
-    givemecustomPOWER
+    rofimoji
+        (rofi-power-menu.overrideAttrs (oldAttrs: {
+      postInstall = (oldAttrs.postInstall or "") + ''
+        target=$out/bin/rofi-power-menu
+        substituteInPlace $target \
+          --replace 'texts[lockscreen]="lock screen"' 'texts[lockscreen]="锁定屏幕"' \
+          --replace 'texts[switchuser]="switch user"' 'texts[switchuser]="切换用户"' \
+          --replace 'texts[logout]="log out"'         'texts[logout]="退出登录"' \
+          --replace 'texts[Suspend]="suspend"'        'texts[Suspend]="挂起"' \
+          --replace 'texts[hibernate]="hibernate"'    'texts[hibernate]="休眠"' \
+          --replace 'texts[reboot]="reboot"'          'texts[reboot]="重启"' \
+          --replace 'texts[shutdown]="shut down"'     'texts[shutdown]="关机"'
+
+        substituteInPlace $target \
+          --replace '"Yes, ' '"确认 ' \
+          --replace '"No, cancel"' '"取消操作"'
+
+        substituteInPlace $target \
+          --replace 'prompt\x1fPower menu"' 'prompt\x1f电源菜单"' \
+          --replace 'prompt\x1fAre you sure"' 'prompt\x1f再次确认"'
+      '';
+    }))
   ];
 }
