ee57b4ae40f8b3342b51170ca4b451762a49e865
dale <dale@e.e>
2026-02-09 12:56:31 +0800

    Normal update

--- generic/modules/media-player.nix
+++ generic/modules/media-player.nix
@@ -10,10 +10,4 @@
   gst_all_1.gst-libav
   glib-networking 
 ];
-  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
-  gst-plugins-good
-  gst-plugins-bad
-  gst-plugins-ugly
-  gst-libav
-  ]);
 }
