598b000f693f1c66f42c416cebea058efed71b89
dale <dale@e.e>
2026-02-09 19:15:41 +0800

    Normal update

--- generic/modules/wrappers/themeFallback.nix
+++ generic/modules/wrappers/themeFallback.nix
@@ -4,22 +4,23 @@
   (
     final: prev: 
     let     
-    themeFallback = pkg: prev.symlinkJoin {
-    name = "${pkg.pname or pkg.name}";
-    paths = [ pkg ];
-    nativeBuildInputs = [ final.makeWrapper ];
-    postBuild = ''
-      wrapProgram $out/bin/${pkg.pname or pkg.name} \
-        --set XDG_CONFIG_HOME "\$HOME/.config/gtk-4.0-isolated"
-      '';
-      };
+      themeFallback = pkg: prev.symlinkJoin {
+      name = "${pkg.pname or pkg.name}-${pkg.version or "0.0.0"}";
+      paths = [ pkg ];
+      nativeBuildInputs = [ final.makeWrapper ];
+  
+      postBuild = ''
+        wrapProgram $out/bin/${pkg.pname or pkg.name} \
+        --run 'export XDG_CONFIG_HOME="$HOME/.config/gtk-isolated"'
+
+    '';
+    };
     in
     {
       clapper = themeFallback prev.clapper;
       loupe = themeFallback prev.loupe;
-      ghostty = themeFallback prev.ghostty;
     }
-  )
-  ];
+ )
+ ];
 
 }
