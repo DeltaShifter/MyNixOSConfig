9a1b657df0f0f5bd84ceb36ae35af46c01dc60be
dale <dale@e.e>
2026-02-09 16:56:32 +0800

    Normal update

--- generic/home.nix
+++ generic/home.nix
@@ -5,19 +5,23 @@ let
    # 调取主机名，方便以后的判断
     currentHostName = osConfig.networking.hostName;
   
-    # 让部分主题不兼容的应用回归默认
+    # 让部分主题不兼容的应用回归默认主题
     Adwaitar = pkg: pkgs.symlinkJoin {
     name = "${pkg.name}-adwaita";
     paths = [ pkg ];
     nativeBuildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
       wrapProgram $out/bin/${pkg.pname or pkg.name} \
-    --set XDG_CONFIG_HOME "\$HOME/.config/gtk-4.0-isolated"
-    mkdir -p $out/share/applications
-      for f in ${pkg}/share/applications/*.desktop; do
+    --set XDG_CONFIG_HOME "\$HOME/.config/gtk-4.0-isolated" # 给这些应用指定一个默认的GTK配置文件
+     mkdir -p $out/share/applications # 接下来我要把.desktop里面的exec指向也改到包装以后的路径
+      for f in ${pkg}/share/applications/*.desktop; do # 遍历所有desktop文件
         target="$out/share/applications/$(basename "$f")"
-        rm -f "$target"
+        # 先找到原有的链接,拼好链，用$out/share/applications加切掉路径保留文件名的$f
+        rm -f "$target" # 断链让symlikJoin生效
         sed "s|^Exec=[^ ]*\(.*\)|Exec=$out/bin/${pkg.pname or pkg.name}\1|g" "$f" > "$target"
+        # 找到Exec=（空格）开头的行，把第一个空格之后的所有内容（也就是二进制命令后面的所有参数）抓起来
+        # 内容放入寄存1，开始拼好令，替换为Exec=加包装后的路径+寄存1（也就是参数）
+        # 拼完后丢入$target,大功告成
       done
     '';
     };
@@ -44,6 +48,7 @@ in
       
     ]
      ++ (map Adwaitar [
+    #此处填写需要使用默认主题的应用
     clapper
     ghostty
     loupe
@@ -75,7 +80,7 @@ in
   programs.home-manager.enable = true;
 
   home.activation = {
-    # Niri 注入include脚本
+    # Niri 注入include脚本,因为Niri的配置不止一个程序管理
     ensureNiriInclude = lib.hm.dag.entryAfter ["writeBoundary"] ''
       CONFIG_FILE="$HOME/.config/niri/config.kdl"
       INCLUDE_LINE='include "my-custom.kdl"'
