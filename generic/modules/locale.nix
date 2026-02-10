3619c482574a56adebf48e3bb3b8a048a9fa9e32
dale <dale@e.e>
2026-02-11 01:30:22 +0800

    Normal update

--- generic/modules/locale.nix
+++ generic/modules/locale.nix
@@ -35,8 +35,8 @@
 
   environment.sessionVariables = {
     XMODIFIERS = "@im=fcitx";
-    QT_IM_MODULE = "fcitx";
-    GTK_IM_MODULE = "fcitx";
+  #  QT_IM_MODULE = "fcitx";
+  #  GTK_IM_MODULE = "fcitx";
     NIXOS_OZONE_WL = 1;
     ELECTRON_OZONE_PLATFORM_HINT = "auto";
 
