08fc92e979fead40f0dfdd486ba0d365a0f2f906
dale <dale@e.e>
2026-02-11 10:52:15 +0800

    Normal update

--- generic/modules/media-player.nix
+++ generic/modules/media-player.nix
@@ -9,5 +9,8 @@
   gst_all_1.gst-plugins-ugly  # SMB 支持通常在这里
   gst_all_1.gst-libav
   glib-networking 
-];
+  ];
+  environment.sessionVariables = {
+    GST_PLUGIN_SYSTEM_PATH_1_0 = "/run/current-system/sw/lib/gstreamer-1.0";
+  };
 }
