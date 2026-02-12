4a766d05accad0ada7499e7537ffcad8e3d64b2a
dale <dale@e.e>
2026-02-12 17:58:57 +0800

    Normal update

--- devices/Optiplex9020m/configuration.nix
+++ devices/Optiplex9020m/configuration.nix
@@ -65,9 +65,9 @@
   # Enable CUPS to print documents.
   services.printing = {
     enable = true;
-    drivers = with pkgs;[
-      cups-fillers
-      cups-browsed
+    drivers = with pkgs; [
+    cups-filters
+    cups-browsed
     ];
   };
   services.avahi = {
