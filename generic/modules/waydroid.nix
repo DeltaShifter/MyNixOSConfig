46e619435ab8c28bda38e0b98ca32f745cc3b338
dale <dale@e.e>
2026-02-20 09:52:43 +0800

    Normal update

--- generic/modules/waydroid.nix
+++ generic/modules/waydroid.nix
@@ -3,6 +3,7 @@
 {
   virtualisation.waydroid.enable = true;
   virtualisation.lxc.enable = true;
+  networking.firewall.trustedInterfaces = [ "waydroid0" ];
   environment.systemPackages = with pkgs; [
     waydroid-helper
   ];
