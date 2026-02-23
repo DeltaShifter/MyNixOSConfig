db63b8fc19e5d74a57b77ae89702185c1d3ca049
dale <dale@email.e>
2026-02-23 22:54:15 +0800

    Normal update

--- generic/modules/wps.nix
+++ generic/modules/wps.nix
@@ -1,29 +1,23 @@
 { config, pkgs, lib, ... }:
 
-let
-  isX1c = config.networking.hostName == "X1c";
-
-  # 针对 wpsoffice-cn 的定制包装
-  myWps = if isX1c then pkgs.symlinkJoin {
-    name = "wpsoffice-cn-wrapped";
-    paths = [ pkgs.wpsoffice-cn ]; # 包含原包所有文件（share, lib等）
-    nativeBuildInputs = [ pkgs.makeWrapper ];
-    postBuild = ''
-      # 遍历 bin 目录下所有可执行文件进行包装
-      # 这样能确保 wps, et, wpp 以及相关的辅助工具都带上 DPI 参数
-      for binary in $out/bin/*; do
-        # 必须先删掉 symlink 才能创建 wrapper 脚本，否则会循环引用
-        if [ -L "$binary" ]; then
-          real_prog=$(readlink -f "$binary")
-          rm "$binary"
-          makeWrapper "$real_prog" "$binary" --set QT_FONT_DPI "144"
-        fi
-      done
-    '';
-  } else pkgs.wpsoffice-cn;
-
-in {
-  environment.systemPackages = [
-    myWps
+{
+  nixpkgs.overlays = [
+    (final: prev: {
+      wpsoffice-cn = if config.networking.hostName == "X1c" 
+        then prev.wpsoffice-cn.overrideAttrs (oldAttrs: {
+          nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
+          postFixup = (oldAttrs.postFixup or "") + ''
+            for bin in $out/bin/*; do
+              wrapProgram "$bin" --set QT_FONT_DPI "144"
+            done
+          '';
+        })
+        else prev.wpsoffice-cn; 
+    })
   ];
+
+  environment.systemPackages = with pkgs; [
+    wpsoffice-cn
+  ];
+
 }
