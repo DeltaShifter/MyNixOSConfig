54c0a34068fc798493ea222f8ed9ed1d0e8474d3
dale <dale@e.e>
2026-02-26 12:32:10 +0800

    Normal update $(%Y-%m-%d %H:%M:%S)

--- generic/modules/fish.nix
+++ generic/modules/fish.nix
@@ -10,7 +10,7 @@
       nixupd = "nh os switch ~/.MyNixConf/ && nixsync";
       flakeupd = "nix flake update --flake ~/.MyNixConf/";
       nixclean = "sudo nix-collect-garbage -d";
-      nixsync = "git --git-dir=$HOME/.MyNixConf/.git --work-tree=$HOME/.MyNixConf add -A && git --git-dir=$HOME/.MyNixConf/.git --work-tree=$HOME/.MyNixConf commit -m 'Normal update $(%Y-%m-%d %H:%M:%S)' && git --git-dir=$HOME/.MyNixConf/.git --work-tree=$HOME/.MyNixConf push";
+      nixsync = "git --git-dir=$HOME/.MyNixConf/.git --work-tree=$HOME/.MyNixConf add -A && git --git-dir=$HOME/.MyNixConf/.git --work-tree=$HOME/.MyNixConf commit -m \'Normal update $(%Y-%m-%d %H:%M:%S)\' && git --git-dir=$HOME/.MyNixConf/.git --work-tree=$HOME/.MyNixConf push";
       nixcachix =''nix path-info -r /run/current-system | cachix push dale-nix-cachix'';
       proxyon = "export http_proxy=http://127.0.0.1:20172 https_proxy=http://127.0.0.1:20172 && curl -I --connect-timeout 3 https://www.google.com";
       ff = "fastfetch --config hypr.jsonc";
