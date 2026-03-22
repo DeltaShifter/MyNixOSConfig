8f269b6dd45d09456eae612f8774262b77fffa0d
dale <dale@email.e>
2026-03-22 22:07:15 +0800

    Update 2026-03-22 22:07:15

--- generic/modules/wps.nix
+++ generic/modules/wps.nix
@@ -1,23 +1,23 @@
 { config, pkgs, pkgs-stable, lib, ... }:
 
-{
-  nixpkgs.overlays = [
-    (final: prev: {
-    wpsoffice-cn = if config.networking.hostName == "X1c" 
-      then prev.wpsoffice-cn.overrideAttrs (oldAttrs: {
-        nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
-        postFixup = (oldAttrs.postFixup or "") + ''
-          for bin in $out/bin/*; do
-            wrapProgram "$bin" \
-            --set QT_FONT_DPI "144" \
-          done
-        '';
-      })
-    else prev.wpsoffice-cn;
-  })
-];
 
+let
+  wpsoffice-cn-dpi = pkgs-stable.wpsoffice-cn.overrideAttrs (oldAttrs: {
+    nativeBuildInputs = (oldAttrs.nativeBulildInputs or [ ]) ++ [ pkgs.makeWrapper ];
+    postFixup = (oldAttrs.postFixup or "") + ''
+      for bin in $out/bin/*;do
+      wrapProgram "$bin" --set QT_FONT_DPI "144"
+      done
+    '';
+  });
+in
+{
   environment.systemPackages = [
-    pkgs-stable.wpsoffice-cn
+    (if
+      config.networking.hostName == "X1c"
+    then
+      wpsoffice-cn-dpi
+    else
+      pkgs-stable.wpsoffice-cn)
   ];
 }
